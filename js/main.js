eee40f05bc3fa605fc79ac4f192a4561fba2bb54
begzod11 <begzod8039@gmail.com>
2022-04-08 21:06:41 +0500

    git initial

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var a = alert("xush kelibsiz");
+var name = prompt("ismingizni kiriting");
+var fullName = prompt("familyangizni kiriting");
+var age = prompt("yoshingiz nechida");
+var job = prompt("ish/o'qish joyingiz");
+var thank = alert("raxmat oka");
+
+console.log("sizning ismingiz" + name + "\n" + "sizning familyangiz" + fullName + "\n" + age + "yoshingiz nechida" + "\n" + "ish yoki oqish joyingiz" + job);
+
+
